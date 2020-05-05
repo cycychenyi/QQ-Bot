85207eb3b7fab0ab23449a36f66fb3bd0e734c31
cycychenyi <chenyi111@whu.edu.cn>
2020-05-05 15:58:21 +0800

    chore: 补充随机题目的功能

--- bot/plugins/leetcode/get_image.py
+++ bot/plugins/leetcode/get_image.py
@@ -4,10 +4,15 @@
 import os
 
 from private_config import coolq_image_dir
+import random
 
 
 async def get_image(question_id: str) -> str:
     images_dir = os.path.join(coolq_image_dir, 'leetcode')
+    if not int(question_id):
+        image = random.sample(os.listdir(images_dir), 1)[0]
+        file_path = os.path.join('leetcode', image)
+        return f'[CQ:image,file={file_path}]'
     for image in os.listdir(images_dir):
         if int(question_id) == int(image[:4]):
             file_path = os.path.join('leetcode', image)
